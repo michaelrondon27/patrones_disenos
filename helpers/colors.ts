6d18529f12fd9d4e020d992247e93ded76d42759
Michael Rondon <mrondon72@gmail.com>
2025-01-09 20:57:54 +0100

    5. Preparación de proyecto

---
+++ helpers/colors.ts
@@ -0,0 +1,15 @@
+export const COLORS = {
+    black : "color: black",
+    blue  : "color: blue",
+    brown : "color: brown",
+    cyan  : "color: cyan",
+    gray  : "color: gray",
+    green : "color: green",
+    orange: "color: orange",
+    pink  : "color: pink",
+    purple: "color: purple",
+    red   : "color: red",
+    yellow: "color: yellow",
+    violet: "color: violet",
+    white : "color: white"
+};
