fda4f5a65dcae3f1abfddccbddedcf9cfd76d448
Michael Rondon <mrondon72@gmail.com>
2025-01-13 23:14:24 +0100

    13. Factory Method - Patrón

--- 01-creacionales/02-factory-method.ts
+++ 01-creacionales/02-factory-method.ts
@@ -12,3 +12,55 @@
  * https://refactoring.guru/es/design-patterns/factory-method
  *
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+interface Hamburger {
+
+    prepare(): void;
+
+}
+
+class ChickenHamburger implements Hamburger {
+    
+    prepare(): void {
+        console.log("Preparando una hamburguesa de %cpollo", COLORS.yellow);
+    }
+
+}
+
+class BeefHamburger implements Hamburger {
+    
+    prepare(): void {
+        console.log("Preparando una hamburguesa de %res", COLORS.brown);
+    }
+
+}
+
+abstract class Restaurant {
+
+    abstract createHamburger(): Hamburger;
+
+    orderHamburger(): void {
+        const hamburger: Hamburger = this.createHamburger();
+        
+        hamburger.prepare();
+    }
+
+}
+
+class ChickenRestaurant extends Restaurant {
+
+    override createHamburger(): Hamburger {
+        return new ChickenHamburger();
+    }
+
+}
+
+class BeefRestaurant extends Restaurant {
+
+    override createHamburger(): Hamburger {
+        return new BeefHamburger();
+    }
+
+}
