7a91e34583c33dce72d1f4f5b712bcd17720933f
Michael Rondon <mrondon72@gmail.com>
2025-01-14 00:46:13 +0100

    20. Abstract Factory - Solución a la tarea

--- 01-creacionales/03.2-abstract-factory.ts
+++ 01-creacionales/03.2-abstract-factory.ts
@@ -13,6 +13,8 @@
  * https://refactoring.guru/es/design-patterns/abstract-factory
  */
 
+import { COLORS } from "../helpers/colors.ts";
+
 /**
  * !Instrucciones:
  	1.Completen las Clases de Productos:
@@ -39,39 +41,69 @@ interface Engine {
 }
   
 // 2. Clases Concretas de Productos  
-class ElectricCar {
-    // Implementación del método assemble
-    // 'Ensamblando un auto eléctrico'
+class ElectricCar implements Vehicle {
+    
+    assemble(): void {
+        console.log("Ensamblando un auto %celéctrico", COLORS.blue);
+    }
+
 }
   
-class GasCar {
-    // Implementación del método assemble
-    // 'Ensamblando un auto de combustión'
+class GasCar implements Vehicle {
+    
+    assemble(): void {
+        console.log("Ensamblando un auto de %ccombustión", COLORS.brown);
+    }
+
 }
   
-class ElectricEngine {
-    // Implementación del método start
-    // 'Arrancando motor eléctrico'
+class ElectricEngine implements Engine {
+    
+    start(): void {
+      console.log("Arrancando motor %celéctrico", COLORS.blue);
+    }
+
 }
   
-class GasEngine {
-    // Implementación del método start
-    // 'Arrancando motor de combustión'
+class GasEngine implements Engine {
+    
+    start(): void {
+        console.log("Arrancando motor de %ccombustión", COLORS.brown)
+    }
+
 }
   
 // 3. Interfaz de la Fábrica Abstracta  
 interface VehicleFactory {
+
     createVehicle(): Vehicle;
     createEngine(): Engine;
+
 }
   
 // 4. Clases Concretas de Fábricas  
 class ElectricVehicleFactory implements VehicleFactory {
-    // Implementación de los métodos createVehicle y createEngine
+
+    createEngine(): Engine {
+        return new ElectricEngine()
+    }
+
+    createVehicle(): Vehicle {
+        return new ElectricCar();
+    }
+
 }
   
 class GasVehicleFactory implements VehicleFactory {
-    // Implementación de los métodos createVehicle y createEngine
+    
+    createEngine(): Engine {
+        return new GasEngine()
+    }
+
+    createVehicle(): Vehicle {
+        return new GasCar();
+    }
+
 }
   
 // 5. Código Cliente  
