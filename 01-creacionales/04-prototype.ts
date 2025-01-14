cd6a2889eaaa9bc58f13095087a697e6f9310c41
Michael Rondon <mrondon72@gmail.com>
2025-01-14 01:03:03 +0100

    21. Prototype - Patrón

--- 01-creacionales/04-prototype.ts
+++ 01-creacionales/04-prototype.ts
@@ -9,3 +9,45 @@
  * 
  * https://refactoring.guru/es/design-patterns/prototype
  */
+
+class Document {
+
+    public author: string;
+    public title : string;
+    
+    private content: string;
+    
+    constructor(title: string, content: string, author: string) {
+        this.author = author;
+        this.content = content;
+        this.title = title;
+    }
+
+    clone(): Document {
+        return new Document(this.title, this.content, this.author);
+    }
+
+    displayInfo(): void {
+        console.log(`
+            Title: ${ this.title }
+            Content: ${ this.content }
+            Author: ${ this.author }
+        `);
+    }
+
+}
+
+function main() {
+    const document1: Document = new Document('Cotización', '500 dólares', 'Michael');
+    
+    console.log({ document1 });
+    document1.displayInfo();
+
+    const document2: Document = document1.clone();
+    document2.title = "Nueva Cotización";
+
+    console.log({ document2 });
+    document2.displayInfo();
+}
+
+main();
