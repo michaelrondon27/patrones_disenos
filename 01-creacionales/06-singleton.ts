5a1f0704a907746eefbd5af7bd64ec0f623eaff4
Michael Rondon <mrondon72@gmail.com>
2025-01-14 22:45:42 +0100

    28. Singleton - Patrón

--- 01-creacionales/06-singleton.ts
+++ 01-creacionales/06-singleton.ts
@@ -10,3 +10,51 @@
  *
  * https://refactoring.guru/es/design-patterns/singleton
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+class DragonBalls {
+
+    private static instance: DragonBalls;
+
+    private ballsCollected: number;
+
+    private constructor() {
+        this.ballsCollected = 0;
+    }
+
+    public static getInstance(): DragonBalls {
+        if (!DragonBalls.instance) {
+            DragonBalls.instance = new DragonBalls();
+
+            console.log("%cLas esferas del dragón han sido creadas!", COLORS.green);
+        }
+
+        return DragonBalls.instance;
+    }
+
+    collectBall(): void {
+        if (this.ballsCollected < 7) {
+            this.ballsCollected++;
+
+            console.log(`Esfera recolectada. Total de esferas: ${ this.ballsCollected }`);
+
+            return;
+        }
+
+        console.log("Ya se han recolectado las 7 esferas del Dragón! Invoca a Shenlong")
+    }
+
+    summonShenlong(): void {
+        if (this.ballsCollected === 7) {
+            console.log("Shenlong ha sido invocado, pide tu deseo!");
+
+            this.ballsCollected = 0;
+        
+            return;
+        }
+
+        console.log(`Aún faltan ${ 7 - this.ballsCollected } esferas para invocar a Shenlong`)
+    }
+
+}
